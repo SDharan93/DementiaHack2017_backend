8683bb30a76c23364c0b56cba739f6f6ae3285a4
sdharan93 <shanedharan@gmail.com>
2017-03-05 05:05:11 -0500

    Restructure models

--- segfault_strategies/lahni/models.py
+++ segfault_strategies/lahni/models.py
@@ -3,25 +3,33 @@ from django.db import models
 
 # Move the models to separate files one day...
 class Instructions(models.Model):
-    createdAt = models.DateTimeField(auto_now_add=True),
-    author = models.CharField(max_length=255, default=""), # Make this a user reference one day...
-    intentID = models.CharField(max_length=255),
+    createdAt = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=255, default="") # Make this a user reference one day...
+    intentID = models.CharField(max_length=255, unique=True)
 
 
 class Steps(models.Model):
-    title = models.CharField(max_length=255),
-    message = models.CharField(max_length=255),
+    title = models.CharField(max_length=255, default="")
+    message = models.CharField(max_length=255)
     failureCount = models.IntegerField(default=0)
     media = models.ForeignKey(Instructions, related_name='media')
+    createdAt = models.DateTimeField(auto_now_add=True)
 
 
 # Used to control possible assets
 class Media(models.Model):
-    audio = models.CharField(max_length=255, default=""),
-    video = models.CharField(max_length=255, default=""),
+    audio = models.CharField(max_length=255, default="")
+    video = models.CharField(max_length=255, default="")
     picture = models.CharField(max_length=255, default="")
     step = models.ForeignKey(Steps, related_name='step')
+    createdAt = models.DateTimeField(auto_now_add=True)
 
+
+class Conversation(models.Model):
+    userInput = models.CharField(max_length=255)
+    output = models.CharField(max_length=255, blank=True)
+    question = models.ForeignKey(Instructions, related_name="conversation")
+    createdAt = models.DateTimeField(auto_now_add=True)
 
 # Possible action types. Used to determine the type of question the user is asking
 ACTION_CHOICES = (
@@ -30,5 +38,6 @@ ACTION_CHOICES = (
 
 
 class Action(models.Model):
-    actionType = models.CharField(max_length=50, choices=ACTION_CHOICES),
+    actionType = models.CharField(max_length=50, choices=ACTION_CHOICES, default='chef')
     title = models.CharField(max_length=255)
+    createdAt = models.DateTimeField(auto_now_add=True)
--- segfault_strategies/lahni/views.py
+++ segfault_strategies/lahni/views.py
@@ -11,7 +11,8 @@ class ListInstructions(APIView):
     def get(self, request, format=None):
         instructions = models.Instructions.objects.all()
         serializer = serializers.InstructionSerializer(instructions, many=True)
-        return Response(serializer.data)
+
+        return Response({'something': 'something'})
 
     def post(self, request, format=None):
         serializer = serializers.InstructionSerializer(data=request.data)
