4350f8f7587789946d386eb6c1d5848659ad1a99
sdharan93 <shanedharan@gmail.com>
2017-03-05 03:54:53 -0500

    Finish basic api structure

--- segfault_strategies/lahni/views.py
+++ segfault_strategies/lahni/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from . import models
+from . import serializers
+
+
+class ListInstructions(APIView):
+    def get(self, request, format=None):
+        instructions = models.Instructions.objects.all()
+        serializer = serializers.InstructionSerializer(instructions, many=True)
+        return Response(serializer.data)
+
+
+class DetailIntstructions(APIView):
+    def post(self, request, format=None):
+        serializer = serializers.InstructionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- segfault_strategies/segfault_strategies/urls.py
+++ segfault_strategies/segfault_strategies/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
+
+# All /api/url/v1/... should be in a router but...ya
 urlpatterns = [
+    url(r'^api/v1/instructions/', include('lahni.urls', namespace='interactions')),
     url(r'^admin/', admin.site.urls),
 ]
