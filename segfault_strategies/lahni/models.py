f5dc53b03b11abf6b2ec7caedea0bc16be053f46
sdharan93 <shanedharan@gmail.com>
2017-03-04 23:50:47 -0500

    Create models for conversations

--- segfault_strategies/lahni/models.py
+++ segfault_strategies/lahni/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+# Move the models to separate files one day...
+class Instructions(models.Model):
+    createdAt = models.DateTimeField(auto_now_add=True),
+    Author = models.CharField(max_length=255, default=""), # Make this a user reference one day...
+    intentID = models.CharField(max_length=255),
+
+    class Meta:
+        ordering = ('createdAt',)
+
+
+class Steps(models.Model):
+    title = models.CharField(max_length=255),
+    message = models.CharField(),
+    failureCount = models.IntegerField(default=0)
+    media = models.ForeignKey(Instructions, related_name='media')
+
+
+# Used to control possible assets
+class Media(models.Model):
+    audio = models.CharField(default=""),
+    video = models.CharField(default=""),
+    picture = models.CharField(default="")
+    step = models.ForeignKey(Steps, related_name='step')
+
+
+# Possible action types. Used to determine the type of question the user is asking
+ACTION_CHOICES = (
+    ('chef', 'recipeRecommendation'),
+)
+
+
+class Action(models.Model):
+    actionType = models.CharField(max_length=50, choices=ACTION_CHOICES),
+    title = models.CharField(max_length=255)
